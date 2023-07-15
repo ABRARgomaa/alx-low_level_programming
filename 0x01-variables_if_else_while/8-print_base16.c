53e0969e26ed7fc4b874da6e85dbb1db10d7e29e
ABRARgomaa <abrargomaa111@gmail.com>
2023-07-14 19:31:54 -0700

    h

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always(0)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 102)
+	{
+		putchar(n);
+		if (n == 57)
+			n +=39;
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
