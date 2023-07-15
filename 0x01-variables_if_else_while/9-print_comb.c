ea0381898a0aae8241763527a302c9bbd55bde1c
ABRARgomaa <abrargomaa111@gmail.com>
2023-07-14 19:43:35 -0700

    h

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Enrty Point
+ * Return: Always(0)
+ */
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9' + '0')
+	{
+		putchar(n, );
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
