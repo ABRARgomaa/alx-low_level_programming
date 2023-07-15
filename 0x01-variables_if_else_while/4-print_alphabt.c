67255990e3abd122d2469b1101149f8c222a3a77
ABRARgomaa <abrargomaa111@gmail.com>
2023-07-14 17:32:00 -0700

    h

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always(0)
+ */
+int main(void)
+{
+	char ch = 'a'
+	while (ch <='z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	char c = 'A'
+		while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
+	
