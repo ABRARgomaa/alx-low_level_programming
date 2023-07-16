d56fb21cc90e13bdf6cd3a8d322d9c925650dc9c
ABRARgomaa <abrargomaa111@gmail.com>
2023-07-15 20:24:46 -0700

    h

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,7 @@ int main(void)
 			{
 				for (d = 0; d < 10; d++)
 				{
-					if (a == 0 && b == 0 && c == 0 $$ d ==0)
+					if (a == 0 && b == 0 && c == 0 && d ==0)
 						continue;
 					putchar(a + '0');
 					putchar(b + '0');
