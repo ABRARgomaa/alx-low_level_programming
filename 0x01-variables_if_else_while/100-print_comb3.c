17b591ccad510cedafdda52c83588e4a8a4d6190
ABRARgomaa <abrargomaa111@gmail.com>
2023-07-15 17:12:49 -0700

    huh

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Enrty Point
+ * Return: Always(0)
+ */
+int main(void)
+{
+	int n, num;
+
+	for (n = 0; n < 9; n++)
+	{
+		for (num = 1; n < 10; num++)
+		{
+			putchar(n + '0');
+			putchar(num + '0');
+		}
+	}
+	return (0);
+}
