1a0c8abfcecef8f322a77ca1a2d20e1411e470d9
ABRARgomaa <abrargomaa111@gmail.com>
2023-07-15 20:07:56 -0700

    h

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always(0)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 9; b++)
+		{
+			for (c = 0; c < 10; c++)
+			{
+				for (d = 1; d < 10; d++)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(' ');
+					putchar(c + '0');
+					putchar(d + '0');
+					if (a == 9 && b == 8 && c == 9 && d == 9)
+					{
+						putchar('\n');
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
+
+
