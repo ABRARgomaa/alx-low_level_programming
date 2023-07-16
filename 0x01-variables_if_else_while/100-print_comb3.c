8dd1e69cf65b102caaf4e910a0b2a5cabc97d3ba
ABRARgomaa <abrargomaa111@gmail.com>
2023-07-15 17:23:13 -0700

    huh

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,14 @@ int main(void)
 
 	for (n = 0; n < 9; n++)
 	{
-		for (num = 1; n < 10; num++)
+		for (num = 1; num < 10; num++)
 		{
 			putchar(n + '0');
 			putchar(num + '0');
 		}
 	}
+	putchar(',')
+	putchar(' ')
+	putchar('\n')
 	return (0);
 }
