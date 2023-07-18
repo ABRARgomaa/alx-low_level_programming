5f98fda6e2ab2e2972d64be233e3cfcc84cb313b
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-18 08:48:00 +0000

    h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - good
+ * Return: nothing
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar('c');
+	}
+	putchar('\n');
+}
