1f916bbde4b3a6b4f686a103e063cafcf85c82b4
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-18 22:25:27 +0000

    h

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include "main.h"
+#include <ctype.h>
 /**
- *
+ * _isalpha - good
+ * @c:c
+ * Return: (0)
  */
 int _isalpha(int c)
 {
-	if (c == 'a' || c <= 'z' || c == 'A' || c <= 'Z' || c != ';')
+	if (isalpha(c))
 	{
 		return (1);
 	}
