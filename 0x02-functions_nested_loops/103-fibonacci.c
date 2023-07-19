000702a8bbf107b50fb9ee27535fc15fd4847d72
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-19 14:26:33 +0000

    h

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: (0)
+ */
+int main(void)
+{
+	unsigned long n1 = 0, n2 = 1, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = n1 + n2;
+		if (sum > 4000000)
+			break;
+		if ((sum % 2) == 0)
+			total_sum += sum;
+		n1 = n2;
+		n2 = sum;
+	}
+	printf("%.0f\n", total_sum);
+	return (0);
+}
