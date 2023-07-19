b87da35b49ea2aef8b64b07c5618899216d5c288
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-19 03:15:04 +0000

    h

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,24 +8,18 @@ void print_to_98(int n)
     int c;
     if (n > 98)
     {
-        for (c = n; c >= 98; c--)
+        for (c = n; c > 98; c--)
         {
-            _putchar(n + '0');
-            _putchar(',');
-            _putchar(' '); 
+           printf("%d, ", c);
         }
     }
-    else if (n == 98)
-    _putchar(n + '0');
+    
     else
     {
-        for (c = n; c <= 98; c++)
+        for (c = n; c < 98; c++)
         {
-           _putchar(n + '0');
-           _putchar(',');
-           _putchar(' ');
-
+            printf("%d, ", c);
         }
     }
-    _putchar('\n');
+    printf("89\n");
 }
