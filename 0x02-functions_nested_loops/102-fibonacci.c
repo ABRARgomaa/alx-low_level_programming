ba1cc9070e6fcc658db949e7c8527086bc19758b
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-19 14:03:59 +0000

    h

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: (0)
+ */
+int main(void)
+{
+	int n;
+	unsigned long n1 = 0, n2 = 1, sum;
+	for (n = 0; n < 50; n++)
+	{
+		sum = n1 + n1;
+		printf("%lu", sum);
+		n1 = n2;
+		n2 = sum;
+		if (n == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
