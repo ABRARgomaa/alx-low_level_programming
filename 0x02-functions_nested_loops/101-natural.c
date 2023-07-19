e7ca416467ef89efb584674ab5aa939faa9a11b9
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-19 13:42:06 +0000

    h

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: (0)
+ */
+int main(void)
+{
+	int sum, num;
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
