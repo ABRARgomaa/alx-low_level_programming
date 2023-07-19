de5ccf51cdbe9bb7128cfd18924405a317a7fe98
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-19 02:00:39 +0000

    h

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,17 +2,18 @@
 #include "main.h"
 /**
  * times_table - good
-*/
+ */
 void times_table(void)
 {
-    int a, b, c;
+	int a, b, c;
 
-    for (a = 0; a < 10; a++)
-    {
-        for (b = 0; b < 10; b++)
-        {
-            c = a * b;
-            printf("%d,..\n", c);
-        }
-    }
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			c = a * b;
+			printf("%d,..", c);
+		}
+		printf("$\n");
+	}
 }
