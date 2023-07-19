e84d3e93a58bcd519d63d93d4e6bc94947424a4f
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-19 02:56:32 +0000

    h

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - print_
+*/
+void print_to_98(int n)
+{
+    int c;
+    if (n > 98)
+    {
+        for (c = n; c >= 98; c--)
+        {
+            printf("%d", c);   
+        }
+    }
+    else if (n == 98)
+    printf("%d", n);
+    else
+    {
+        for (c = n; c <= 98; c++)
+        {
+           printf("%d", c); 
+        }
+    }
+    printf('\n');
+}
