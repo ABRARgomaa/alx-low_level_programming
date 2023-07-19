9c3555b1454b98ca77b03778019e3cadb164ed4d
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-19 02:27:24 +0000

    h

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,17 @@ void times_table(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b < 10; b++)
+		_putchar(48);
+		for (b = 1; b < 10; b++)
 		{
-			c = a * b;
-			printf("%d, ", c);
-			if (c <= 9)
+			_putchar(',');
 			_putchar(' ');
+			c = a * b;
+			if (c <= 9)
+				_putchar(' ');
+			else
+				_putchar((c / 10) + 48);
+			_putchar((c % 10) + 48);
 		}
 		printf("\n");
 	}
