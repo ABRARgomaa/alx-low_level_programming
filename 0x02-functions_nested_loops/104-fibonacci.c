0d3d4bca223293f9a6f5d54082b9aa65d3eb0678
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-19 15:18:44 +0000

    h

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,18 +5,40 @@
  */
 int main(void)
 {
-	int n;
-	unsigned long n1 = 0, n2 = 1, sum;
-	for (n = 0; n < 98; n++)
+
+	int count, initialos;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+
+	for (count = 1; count < 98; count++)
 	{
-		sum = n1 + n2;
-		printf("%lu", sum);
-		n1 = n2;
-		n2 = sum;
-		if (n == 98)
-			printf("\n");
-		else
+
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initialos = numLength(mx) - 1 - numLength(f1);
+
+		while (f1o > 0 && initialos > 0)
+		{
+
+			printf("%d", 0);
+			initialos--;
+		}
+
+		printf("%lu", f1);
+
+		sum = (f1 + f2) % mx;
+		sumo = f1o + f2o + (f1 + f2) / mx;
+
+		f1 = f2;
+		f1o = f2o;
+
+		f2 = sum;
+
+		f2o = sumo;
+
+		if (count != 98)
 			printf(", ");
+		else
+			printf("\n");
 	}
 	return (0);
 }
