3ec459c577ed6fb43b41db9635a0e183f4171e47
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-19 09:21:34 +0000

    h

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - good
+ * @n: good
+*/
+void print_times_table(int n)
+{
+    int a, b, c;
+    
+    if (n > 15 && n < 0)
+    break;
+    for (a = 0; a <= n; a++)
+    {
+        for (b = 0; b <=n; b++)
+        {
+            c = a * b;
+            printf("%d, ", c);
+            if (c <= 9)
+            {
+                printf(' ');
+            }
+            
+        }
+        printf('\n');
+    }
+}
