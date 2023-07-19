ceb6a0e91b590e67b626f74025ecd1d41eb288a2
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-19 09:57:35 +0000

    h

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,16 +11,18 @@ void print_times_table(int n)
 	for (a = 0; a <= n; a++)
 	{
 		_putchar(48);
-		for (b = 1; b <=n; b++)
+		for (b = 1; b <= n; b++)
 		{
 			_putchar(',');
 			_putchar(' ');
 			c = a * b;
 			if (c <= 9)
-			_putchar(' ');
-			_putchar(' ');
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
 			else
-			_putchar((c / 10) + 48);
+				_putchar((c / 10) + 48);
 			_putchar((c % 10) + 48);
 		}
 		_putchar('\n');
