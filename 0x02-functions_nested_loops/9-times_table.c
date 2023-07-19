8a95b326a40daf361ea4bdb95258fb2926a43ec7
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-19 01:58:02 +0000

    h

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - good
+*/
+void times_table(void)
+{
+    int a, b, c;
+
+    for (a = 0; a < 10; a++)
+    {
+        for (b = 0; b < 10; b++)
+        {
+            c = a * b;
+            printf("%d,..\n", c);
+        }
+    }
+}
