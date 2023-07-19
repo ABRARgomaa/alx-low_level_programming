9a78957b695c20568c3b5618a43ec7ecbf829859
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-19 01:17:11 +0000

    h

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,8 +9,10 @@ void jack_bauer(void)
     for (a = 0; a < 24; a++)
     {
         for (b = 0; b < 60; b++)
+        {
         printf("%02d", a);
         _putchar(':');
         printf("%02d", b);
+        }
     }
 }
