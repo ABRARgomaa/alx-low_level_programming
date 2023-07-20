2766c0497c2570c29dca03254c4c20d1c71a31a1
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-20 10:59:22 +0000

    huh

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - good
+ */
+void print_line(int n)
+{
+	int c;
+
+	if (n <= 0)
+		putchar('\n');
+	else
+	{
+		for (c = 1; c <= n; c++)
+		{
+			putchar('_');
+		}
+		putchar('\n');
+	}
+}
