aa000066df759262ded91b0bc71d2bcf14497c52
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-20 01:58:19 +0000

    huh

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,6 @@
  */
 
 void print_remaining_days(int month, int day, int year)
-
 {
     /**
      * leap year when it can be divided
@@ -19,7 +18,7 @@ void print_remaining_days(int month, int day, int year)
     */
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
