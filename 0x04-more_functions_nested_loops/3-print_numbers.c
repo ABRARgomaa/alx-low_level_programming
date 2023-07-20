03bb72c4210c6066876a41d102343373db6251bc
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-20 06:05:50 +0000

    huh

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - good
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+}
