2027d130a1653d12f2293130396b7e70485bddd1
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-24 23:16:30 +0000

    huh

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - doo
+ * @str: doo
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar("\n");
+}
