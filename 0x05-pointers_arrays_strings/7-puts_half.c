0e3bdbe6194d0ab5719b164d4891098fdc03b88f
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-25 02:55:08 +0000

    huh

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,23 +6,18 @@
  */
 void puts_half(char *str)
 {
-	int length = 0;
+	int length, start_index, i;
 
-	// Calculate the length of the string
+length = 0
 	while (str[length] != '\0')
 	{
 		length++;
 	}
-
-	// Calculate the starting index for the second half of the string
-	int start_index = (length - 1) / 2;
-
-	// Print the second half of the string
-	for (int i = start_index; i < length; i++)
+	start_index = (length - 1) / 2;
+	for (i = start_index; i < length; i++)
 	{
 		putchar(str[i]);
 	}
 
-	// Print a new line
 	putchar('\n');
 }
