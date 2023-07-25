cc50b2dfdab44d171a69bf4f55fa1e3f66865867
ABRARgomaa <133330312+ABRARgomaa@users.noreply.github.com>
2023-07-25 02:31:32 +0000

    huh

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts_half - good
+ * @str: good
+ */
+void puts_half(char *str)
+{
+	int l, i;
+	char temp;
+	for (l = 0; str[l] != '\0'; l++)
+		;
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = str[i];
+		str[i] = str[l - 1 - i];
+		str[l - 1 - i] = temp;
+	}
+}
