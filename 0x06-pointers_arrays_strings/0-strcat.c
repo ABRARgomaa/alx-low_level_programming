df0df5bf1fd9c146d9b83e1419fc32297c96ab7c
ABRARgomaa <abrargomaa111@gmail.com>
2023-07-26 05:41:58 -0700

    huh

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *_strcat - good
+ * @dest: good
+ * @src: good
+ * Return: (0)
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (0);
+}
