8f1d1b635ec7156cd0e608cd265f8b7282ca6a30
ABRARgomaa <abrargomaa111@gmail.com>
2023-08-02 15:42:34 -0700

    huh

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - good
+ * @s: good
+ * Return: (0)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		printf("\n");
+	else
+
+	{
+		printf ("%c", *s);
+		_puts_recursion(s+1);
+	}
+}
+
