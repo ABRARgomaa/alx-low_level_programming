517debf9ae0394fbebcae4ccaf7333fdebdacd1e
ABRARgomaa <abrargomaa111@gmail.com>
2023-08-02 17:31:48 -0700

    huh

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen_recursion - good
+ * @s: good
+ * Return: (0)
+ */
+int _strlen_recursion(char *s)
+{
+	int a;
+
+	a = strlen(s);
+	return (a);
+}
+
