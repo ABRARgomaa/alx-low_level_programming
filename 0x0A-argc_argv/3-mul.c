330f1680dfcae297a19967bdff3688a9cbc1a9e1
ABRARgomaa <abrargomaa111@gmail.com>
2023-08-04 16:08:23 -0700

    huh

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - good
+ * @argc: good
+ * @argv: good
+ * Return: (0)
+ */
+int main(int argc, char const **argv)
+{
+	int a;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", a);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
+
