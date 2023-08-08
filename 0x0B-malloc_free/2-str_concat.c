2a22f74318ce8f08da5964f44103456529d4fc4d
ABRARgomaa <abrargomaa111@gmail.com>
2023-08-07 17:11:56 -0700

    huh

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,15 +13,15 @@ char *str_concat(char *s1, char *s2)
 	int size1, size2;
 	int i, j;
 
-	if(s1 == NULL || s2 == NULL)
-		return("");
+	if (s1 == NULL || s2 == NULL)
+		return ("");
 	for (size1 = 0; s1[size1] != '\0'; size1++)
 		;
 	for (size2 = 0; s2[size2] != '\0'; size2++)
 		;
 	a = malloc(size1 + size2 + 1);
 	if (a == NULL)
-		return("");
+		return ("");
 	for (i = 0; i < size1; i++)
 		a[i] = s1[i];
 	for (j = 0; j < size2; j++)
