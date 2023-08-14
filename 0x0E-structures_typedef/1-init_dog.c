53dc35d04e31ec110d1969f18903befda1b05aeb
ABRARgomaa <abrargomaa111@gmail.com>
2023-08-14 16:32:20 -0700

    h

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+#include <string.h>
+/**
+ * init_dog - good
+ * @d: good
+ * @name: good
+ * @age: good
+ * @owner: good
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		strcpy(d->name, name);
+		d->age = age;
+		strcpy(d->owner, owner);
+	}
+}
