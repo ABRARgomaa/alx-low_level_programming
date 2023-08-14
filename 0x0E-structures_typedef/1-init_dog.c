6bb646de76f16cc3ea8cbf63ccea5e09fdb52c5a
ABRARgomaa <abrargomaa111@gmail.com>
2023-08-14 16:41:47 -0700

    h

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,10 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
-	{
-		strcpy(d->name, name);
-		d->age = age;
-		strcpy(d->owner, owner);
-	}
+	if (d == NULL)
+		return;
+	strcpy(d->name, name);
+	d->age = age;
+	strcpy(d->owner, owner);
 }
+
