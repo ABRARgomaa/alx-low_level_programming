692b0b7391eb690ff3a629bb55bf2c21da65eb93
ABRARgomaa <abrargomaa111@gmail.com>
2023-08-16 19:02:38 -0700

    h

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - good
+ * @array: good
+ * @size: good
+ * @cmp: good
+ * Return: (0)
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	for (a = 0; a < size; a++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
+
