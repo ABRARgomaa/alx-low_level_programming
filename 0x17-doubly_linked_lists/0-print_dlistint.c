0a35ad866861ae6d708b4eae9c2cb1bc33f31693
ABRARgomaa <abrargomaa111@gmail.com>
2023-10-12 17:13:11 -0700

    h

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_dlistint - print elements
+ * @h: node
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t c;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		c++;
+	}
+	return (c);
+}
