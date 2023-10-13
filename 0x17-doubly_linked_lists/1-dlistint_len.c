62cecfe23179a15fbbd99c2e6b006b7b018be129
ABRARgomaa <abrargomaa111@gmail.com>
2023-10-12 17:23:20 -0700

    h

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * dlistint_len - return number of elements
+ * @h: node
+ * Return: count
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t c = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		c++;
+	}
+	return (c);
